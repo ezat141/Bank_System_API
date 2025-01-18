f24bba0b109bd0aae113448b096b74df99809695
Ezzat Mohamed <73354781+ezat141@users.noreply.github.com>
2025-01-19 01:21:40 +0200

    security created

---
+++ src/main/java/com/bank/banksystemapi/security/JWTService.java
@@ -0,0 +1,91 @@
+package com.bank.banksystemapi.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
+
+@Service
+public class JWTService {
+    @Value("${application.security.jwt.secret-key}")
+    private String SECRET_KEY;
+
+    @Value("${application.security.jwt.expiration}")
+    private Long jwtEpiration;
+
+
+    public String getUserEmail(String jwtToken) {
+        return extractClaim(jwtToken, Claims::getSubject);
+    }
+
+    // get expiration date from token
+    private Date extractExpiration(String jwtToken) {
+        return extractClaim(jwtToken, Claims::getExpiration);
+    }
+
+    public String generateToken(
+            Map<String, Objects> extraClaims,
+            UserDetails userDetails
+    ){
+
+        String authority = userDetails.getAuthorities().stream()
+                .findFirst()
+                .map(GrantedAuthority::getAuthority)
+                .orElse("");
+
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtEpiration) )
+                .claim("authorities", authority)
+                .signWith(getSignKey(), SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    public String generateToken( UserDetails userDetails){
+        return generateToken(new HashMap<>(),userDetails);
+    }
+    private <T> T extractClaim(String jwtToken, Function<Claims,T> claimResolver) {
+        final Claims allClaims = extractAllClaims(jwtToken);
+        return claimResolver.apply(allClaims);
+    }
+
+    private Claims extractAllClaims(String jwtToken) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(jwtToken)
+                .getBody();
+    }
+
+    private boolean isTokenExpired(String jwtToken) {
+        return extractExpiration(jwtToken).before(new Date());
+    }
+
+    public boolean isTokenValid(String jwtToken, UserDetails userDetails) {
+        String userEmail = getUserEmail(jwtToken);
+        return userEmail.equals(userDetails.getUsername()) && !isTokenExpired(jwtToken) ;
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+
+}
---
+++ src/main/java/com/bank/banksystemapi/security/SecurityConf.java
@@ -0,0 +1,45 @@
+package com.bank.banksystemapi.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConf {
+    private final AuthenticationProvider authenticationProvider;
+    private final JWTFilter jwtAuthFilter;
+
+    public SecurityConf(AuthenticationProvider authenticationProvider, JWTFilter jwtAuthFilter) {
+        this.authenticationProvider = authenticationProvider;
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
+    @Bean
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        // Allow access to Swagger endpoints
+                        .requestMatchers("/bank/auth/**",
+                        "/bank/transaction/**")
+                        .permitAll()
+                        // Any other requests must be authenticated
+                        .anyRequest()
+                        .authenticated())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
