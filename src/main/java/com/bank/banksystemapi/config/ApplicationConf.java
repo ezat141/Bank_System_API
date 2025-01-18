df7b92d9393c9639104f7cb831909c0fe506fb88
Ezzat Mohamed <73354781+ezat141@users.noreply.github.com>
2025-01-19 01:23:33 +0200

    Configuration created

---
+++ src/main/java/com/bank/banksystemapi/config/ApplicationConf.java
@@ -0,0 +1,89 @@
+package com.bank.banksystemapi.config;
+
+import com.bank.banksystemapi.repository.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+@Configuration
+public class ApplicationConf {
+    private final UserRepository userRepository;
+
+    public ApplicationConf(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    // handling login requests and validating user credentials.
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration conf) throws Exception {
+        return conf.getAuthenticationManager();
+    }
+
+    //load user details during authentication.
+    @Bean
+    UserDetailsService userDetailsService(){
+        return username ->
+                userRepository.findByEmail(username)
+                        .orElseThrow( ()-> new UsernameNotFoundException("user not found"));
+    }
+
+    //the logic for authenticating users.
+    @Bean
+    AuthenticationProvider authenticationProvider(){
+
+
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        // Add your frontend origin here or use "*" for allowing all origins (not recommended for production)
+        config.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://localhost:34659"));
+        config.setAllowedHeaders(Arrays.asList(
+                HttpHeaders.ORIGIN,
+                HttpHeaders.CONTENT_TYPE,
+                HttpHeaders.ACCEPT,
+                HttpHeaders.AUTHORIZATION
+        ));
+        config.setAllowedMethods(Arrays.asList(
+                "GET",
+                "POST",
+                "DELETE",
+                "PUT",
+                "PATCH"
+        ));
+        // Optionally, you can add max age, expose headers, etc.
+        // config.setMaxAge(3600L);
+        // config.setExposedHeaders(Arrays.asList("X-Auth-Token", "Authorization"));
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+}
+
